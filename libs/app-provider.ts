285511854d676eb8af3f94c206cbf822ae539289
@enwerro <rock.enwerem@tecnotree.com>
2023-10-03 00:32:36 +0100

    Add provider

---
+++ libs/app-provider.ts
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
+import type { AppProps as NextAppProps } from "next/app";
+import { NextRouter } from "next/router";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement, router: NextRouter) => ReactNode;
+  PageWrapper: (props: NextAppProps) => JSX.Element;
+};
+
+export type AppPropsWithLayout = NextAppProps & {
+  Component: NextPageWithLayout;
+};
