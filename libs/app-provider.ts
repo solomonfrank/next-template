12ecf8d7e2516f2be0ba858c634b0cec8e29aba8
solomonfrank <solomonfrank73@hotmail.com>
2023-10-03 01:04:17 +0100

    update

--- libs/app-provider.ts
+++ libs/app-provider.ts
@@ -4,7 +4,7 @@ import type { AppProps as NextAppProps } from "next/app";
 import { NextRouter } from "next/router";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement, router: NextRouter) => ReactNode;
+  getLayout?: (page: ReactElement, router: NextRouter) => ReactNode; // custom layout
   PageWrapper: (props: NextAppProps) => JSX.Element;
 };
 
